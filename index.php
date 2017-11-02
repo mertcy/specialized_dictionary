6119b773cf212d3aa09e6a552f330334fbe5a2fd
yelensel <selen.yel@hotmail.com>
2017-11-02 23:25:10 +0300

    simple changes on index.php -> main -> content

--- index.php
+++ index.php
@@ -17,65 +17,60 @@
             <div id="index">
                 <p> Index </p>
                     <div class="scrollbar" id="style-2">
-                     <div class="force-overflow">
-                        <ul class="topics">
-                            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        <div class="force-overflow">
+                            <ul class="topics">
+                                <li class="topic">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                                  sed do eiusmod tempor incididunt ut labore et dolore magna
                                  aliqua. Ut enim ad minim veniam,
                                  <!-- bu kısım gündem başlığı için --></li>
-                            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                <li class="topic">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                                  sed do eiusmod tempor incididunt ut labore et dolore magna
                                  aliqua. Ut enim ad minim veniam,
                                  <!-- bu kısım gündem başlığı için --></li>
-                            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                <li class="topic">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                                  sed do eiusmod tempor incididunt ut labore et dolore magna
                                  aliqua. Ut enim ad minim veniam,
                                  <!-- bu kısım gündem başlığı için --></li>
-                            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                <li class="topic">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                                  sed do eiusmod tempor incididunt ut labore et dolore magna
                                  aliqua. Ut enim ad minim veniam,
                                  <!-- bu kısım gündem başlığı için --></li>
-                            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                <li class="topic">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                                  sed do eiusmod tempor incididunt ut labore et dolore magna
                                  aliqua. Ut enim ad minim veniam,
                                  <!-- bu kısım gündem başlığı için --></li>
-                            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                <li class="topic">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                                  sed do eiusmod tempor incididunt ut labore et dolore magna
                                  aliqua. Ut enim ad minim veniam,
                                  <!-- bu kısım gündem başlığı için --></li>
-                            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                <li class="topic">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                                  sed do eiusmod tempor incididunt ut labore et dolore magna
                                  aliqua. Ut enim ad minim veniam,
                                  <!-- bu kısım gündem başlığı için --></li>
-                        </ul>
+                            </ul>
+                        </div>
                     </div>
-             </div>
-
+                </div>   
             </div>
-            <ul class="topics">
-                <li><!-- bu kısım gündem başlığı için --></li>
-                <li><!-- bu kısım gündem başlığı için --></li>
-                <li><!-- bu kısım gündem başlığı için --></li>
-                <li><!-- bu kısım gündem başlığı için --></li>
-                <li><!-- bu kısım gündem başlığı için --></li>
-            </ul>
-                
-        </div>
         <div class="main"><!-- bu kısım main sayfa için -->
             <p><!--main --></p>
             <h1 id="title" data-title="" data-id="" data-slug="" data-vote-url="" data-unvote-url="" data-commet-vote-url="" ><!-- main başlığı --><h1>
             <ul id="entry_list">
-                <li><!-- bu kısım entry için (up dahil) -->
+                <li><!-- bu kısım entry için (up dahil) --> Lorem ipsum dolor sit amet, consectetur
                     <div class="content"><!-- bu kısım entry için -->
-
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore magna
+                        aliqua. Ut enim ad minim veniam,
                     </div>
                     <footer>
                         <div><!-- bu kısım face twitter up kısmı -->
                             <span><!-- bu kısım face twitter kısmı --></span>
                             <span><!-- bu kısım up down kısmı --></span>
-                        </div>
-                            <a class="entry_date"></a>
-                            <a class="entry_author"></a>
+                            <div>
+                                <a class="entry_date">02/11/2017</a>
+                                <a class="entry_author">YelenSel</a>
+                            </div>
+                        </div>                        
                             <div>
                                 <a class="dropdown_toggle"> </a>
                                 <ul class="dropdown_menu_report">
@@ -84,7 +79,6 @@
                                     </li>
                                 </ul>
                             </div>
-                        <div><!-- bu kısım tarih ve kişi kısmı --></div>
                     </footer>
                 </li>
             </ul>
