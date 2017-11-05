947edff53e011a60374e1302b84de1ccde49c75c
oguzmertcaylar <oguzmertcaylar7@gmail.com>
2017-11-06 00:29:05 +0300

    added ajax call using jquery for creating a new entry title

--- entrytitleNew.php
+++ entrytitleNew.php
@@ -2,3 +2,19 @@
     define("TITLE", "home | specialized dictionary");
     include('includes/header.php');
 ?>
+
+<div class="index2"> <!-- bu kısım gündem için -->
+            <div id="index" >
+                <?php    
+                    include('entrytitlepaneHeader.php');
+                    include('entrytitlepane.php');
+                ?>
+            </div>   
+        </div>
+        <?php
+            echo "entries";
+        ?>
+
+<?php
+    include('includes/footer.php');
+?>
--- entrytitlepaneHeader.php
+++ entrytitlepaneHeader.php
@@ -8,7 +8,23 @@
                                     f1();       
                                 ?>
                             </a>
-                        <img src="img/pen.png" title="open new entry title" width="30" height="30" align="right" onclick="entrytitleNew.php" />
+                        
+                            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>                       
+                            <script>
+                                $(document).ready(function(){
+                                    $("img").click(function(){
+                                        $.ajax({
+                                            type: 'POST',
+                                            url: 'entrytitleNew.php',
+                                            success: function(data) {
+                                                $("#home-page").load("entrytitleNew.php");
+                                            }
+                                        });
+                                    });
+                                });
+                            </script>
+
+                        <img type="img" src="img/pen.png" title="open new entry title" width="30" height="30" align="right" />
                         </th>
                     </tr>
                 </table>
