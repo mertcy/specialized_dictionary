e9b6a672c221577c21df781d2e0172c543af4ebb
oguzmertcaylar <oguzmertcaylar7@gmail.com>
2017-11-07 11:27:33 +0300

    added stub database query

---
+++ database/db_stub.sql
@@ -0,0 +1,11 @@
+INSERT INTO specialized_sch.title VALUES(null, 2, 'sabretmenin gerekliligi', null, null, null, null);
+
+INSERT INTO specialized_sch.title VALUES(null, 5, 'neler yapmali', null, null, null, null);
+
+INSERT INTO specialized_sch.entry VALUES(7190, 2, 11,'bugun gunlerden sali', '2017-10-15 12:00:00', '2017-08-15 13:00:00', null, null);
+INSERT INTO specialized_sch.entry VALUES(14585, 2, 12,'sabrin sonu selamettir', '2017-08-15 08:15:00', null, null, null);
+INSERT INTO specialized_sch.entry VALUES(null, 2, 13,'bir okul donemini daha geride biraktik', null, null, null, null);
+
+INSERT INTO specialized_sch.entry VALUES(null, 5, 21,'finallerin yakinda aciklanacagini umuyorum', null, null, null, null);
+INSERT INTO specialized_sch.entry VALUES(null, 5, 22,'bugun bisiklet surecegim ', null, null, null, null);
+INSERT INTO specialized_sch.entry VALUES(null, 5, 23,'umuyorum ki hava guzeldir', null, null, null, null);
